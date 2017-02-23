fd0e201c8e5cd31a046b8fb40ff82b9086357ad8
Jeff <jeff@mechazawa.gtfo>
2017-02-23 13:39:46 -0800

    added some very silly routes

--- app.js
+++ app.js
@@ -7,6 +7,40 @@ app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
 
+let middlewares = [
+  (req, res, next) => {
+    res.x = "First middleware added this string property in 'x' to the response object.";
+    next();
+  },
+  (req, res, next) => {
+    res.y = "Second middleware added a new string property in 'y' to the response object.";
+  }
+];
+
+let middleWareGenerator = function*(req, res, next) {
+  let x = 0;
+  while(true) {
+    yield function(req, res, next) {
+      x++;
+      console.log(`${x}: This is really really silly!`);
+      next();
+    };
+  }
+};
+
+let factory = middleWareGenerator(10);
+
+let middlewares2 = [
+  factory.next().value,
+  factory.next().value,
+  factory.next().value,
+  factory.next().value,
+  factory.next().value,
+  (req, res, next) => {
+    res.send(`<h1>I hope you're happy with yourself...</h1>`);
+  },
+];
+
 app.use('/test1',
        (req, res, next) => {
          res.x = "Hello from first middleware function!";
@@ -19,6 +53,8 @@ app.use('/test1',
        (req, res) => {
          res.send(`<h1>Value of response.x: <br>${res.x}</h1>`);
        });
+
+app.use('/test3', middlewares2);
 
 app.get('/test1', (req, res) => {
   console.log('Above middleware is already using this path, and will not allow requests past this point. This route will never be reached.');
